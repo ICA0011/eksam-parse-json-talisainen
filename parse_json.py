899939f5186fded1eb55b384cc5c0c614d06db11
ALTA <talisainen@users.noreply.github.com>
2022-05-30 07:29:10 +0300

    Update parse_json.py

--- parse_json.py
+++ parse_json.py
@@ -1,9 +1,11 @@
-import json, requests
+import json
+import requests
 
 url = 'http://upload.itcollege.ee/~aleksei/eksam.json'
 
 def parse_json(url):
     
-    # your code here
-    
-    return course_code
+    file_content = json.loads(requests.get(url).text)
+    for course in file_content['courses']:
+        if 'Scripting languages' in course.values():
+            return course['code']
